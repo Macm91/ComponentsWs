54bd2264b5e83208f05fbf54819259154eedc99e
Macm91 <macm91.mc@gmail.com>
2021-09-10 10:40:15 -0400

    created a jokesapi component. need to figure out how to add cors and cors header to the installed apps and middleware to implement the API

--- src/components/JokesAPI/JokesAPI.jsx
+++ src/components/JokesAPI/JokesAPI.jsx
@@ -1,3 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import './JokesAPI.css';
 
+
+// I NEED TO IMPORT SOFTWARE TO USE AXIOS 
+// CORSHEADERS    
+
+
+
+
+class JokesAPI extends Component{
+    state = {
+      joke: []
+        }
+    
+    componentDidMount (){
+        axios.get('https://v2.jokeapi.dev/joke/Programming?type=twopart&amount=5')
+        .then(response => this.setState({
+            joke: response.data
+        }));
+    }
+
+}
+
+
+export default JokesAPI;
